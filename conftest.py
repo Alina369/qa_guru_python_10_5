cb04622318fdce07abb65f3018ac9a9ac0b5b7c6
Alina Lang <lav0903@mail.ru>
2024-01-30 14:01:03 +0300

    Init commit

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ from selene import browser
 def setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+
 
     yield
 
--- test_demoqa.py
+++ test_demoqa.py
@@ -1,17 +1,41 @@
-from selene import browser, be, have
+from selene import browser, be, have, by
+import os
+
 
 def test_registration_form():
-    browser.open("https://demoqa.com/automation-practice-form")
+    browser.open("/")
     browser.element("#firstName").type("Anna")
-    browser.element("[#lastName").type("Ivanova")
-    browser.element("#gender-radio-2").click()
+    browser.element("#lastName").type("Ivanova")
+    browser.element("#userEmail").type("testemail@mail.ru")
+    browser.element('[for="gender-radio-2"]').click()
     browser.element("#userNumber").type("8952333222")
     browser.element("#dateOfBirthInput").click()
     browser.element(".react-datepicker__month-select").element('option[value="2"]').click()
     browser.element(".react-datepicker__year-select").element('option[value="1994"]').click()
     browser.element(".react-datepicker__day--024").click()
+    browser.element("#subjectsInput").should(be.blank).type("a").press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture.form-control-file').send_keys(os.path.abspath('orig.jpg'))
+    browser.element('#currentAddress').type('address')
+    browser.element("#state").should(be.clickable).click()
+    browser.element(by.text('NCR')).should(be.clickable).click()
+    browser.element("#city").should(be.clickable).click()
+    browser.element(by.text('Delhi')).should(be.clickable).click()
     browser.element("#submit").click()
-    browser.element("example-modal-sizes-title-lg").should(have.texts("Thanks for submitting the form"))
+
+
+    browser.element('.table').all('td').even.should(have.texts(
+        'Anna Ivanova',
+        'testemail@mail.ru',
+        'Female',
+        '8952333222',
+        '24 March,1994',
+        'Maths',
+        'Sports',
+        'orig.jpg',
+        'address',
+        'NCR Delhi'))
+
 
 
 
